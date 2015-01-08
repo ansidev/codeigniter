bea3acf320219953ab13065d173962dd7a2e03e4
ansidev <ansidev@gmail.com>
2015-01-08 21:16:15 +0700

    Add home icon for blog

--- application/views/layout/top-nav.php
+++ application/views/layout/top-nav.php
@@ -7,7 +7,10 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/">Devbook</a>
+          <a class="navbar-brand" href="/">
+            <span class="glyphicon glyphicon-home"></span>
+            Devbook
+          </a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
